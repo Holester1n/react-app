5975261473f215d60411921bba69676f3e3fb145
Holester1n <aleksoch2005@gmail.com>
2025-05-01 22:04:40 +0700

    Test commit for auto-deploy

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import MyModal from './components/UI/MyModal/MyModal';
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: 'Description'},
-    {id: 2, title: 'JavaScript 2', body: 'Description'},
-    {id: 3, title: 'JavaScript 3', body: 'Description'},
-    {id: 4, title: 'JavaScript 4', body: 'Description'}
+    {id: 1, title: 'C++', body: 'Description'},
+    {id: 2, title: 'YoptaScript', body: 'Description'},
+    {id: 3, title: 'JavaScript', body: 'Description'},
+    {id: 4, title: 'Python', body: 'Description'}
   ]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);

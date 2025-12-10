2b7b57b6223f431b5f1ebf2e21e5f92e872f441c
Holester1n <aleksoch2005@gmail.com>
2025-12-10 21:00:28 +0700

    добавлена пагинация

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import MyModal from './components/UI/MyModal/MyModal';
 import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
-
+import { useFetching } from './hooks/useFetching';
+import axios from 'axios';
+import { getPagesArray, getPagesCount } from './components/utils/pages';
 function App() {
   const [posts, setPosts] = useState([
     {id: 1, title: 'C++', body: 'Description'},
@@ -18,9 +20,18 @@ function App() {
   ]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  let pagesArray = getPagesArray(totalPages);
 
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPagesCount(totalCount, limit ))
+  })
 
   useEffect(() => {
     fetchPosts()
@@ -31,33 +42,42 @@ function App() {
     setModal(false);
   }
 
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000)
-    
-  }
-
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+  
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts()
   }
   
   return (
     <div className="App">
       <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
         Создать пользователя
-      </MyButton>
+      </MyButton>   
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost}/>
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter}/>
+      {postError &&
+        <h1>Произошла ошибка ${postError}</h1>
+      }
       {isPostsLoading
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
       }
+      <div className='page__wrapper'>
+        {pagesArray.map(p => 
+          <span 
+            onClick={() => changePage(p)}  
+            key={p} 
+            className={page === p ? 'page page__current' : 'page'}
+          >
+            {p}
+          </span>
+        )}
+      </div>
       
     </div>
   );
